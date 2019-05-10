8fe4f2bede6b1df881cc4f7236960c514a10d243
kimohgeun <kog1059@gmail.com>
2019-05-10 14:20:48 +0900

    프로젝트 세팅

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import GlobalStyle from './GlobalStyle';
+
+const App = () => (
+	<>
+		<GlobalStyle />
+    <span>오늘의 일기</span>
+	</>
+);
+
+export default App;
---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+    ${reset}
+    *{
+        box-sizing:border-box;
+    }
+    a{
+        text-decoration:none;
+        color:inherit;
+    }
+    body{
+       font-family: 'Nanum Gothic', sans-serif;
+       font-size: 1rem;
+    }
+`;
+
+export default GlobalStyle;
